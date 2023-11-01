9187243bb52b24bc55c655c14ceb368d80c5df42
Shubh Agarwal <shubhagarwa8888@gmail.com>
2023-11-01 10:03:45 +0530

    Update docs.py

--- docs.py
+++ docs.py
@@ -2,7 +2,7 @@ description = """
 **DBIS Lab project || IMDB Clone || Backend**
 
 Developed by a group of four students:
-    
+
     - Shubh Agarwal(210020047)
     - Saksham Chhimwal(210010046)
     - Shivesh Pandey(2100200044)
@@ -25,18 +25,44 @@ tags_metadata = [
     },
     {
         "name": "Person",
-        "description": "A json is returned"
+        "description": """Endpoints related to persons (actors, directors, writers, etc.).
+        - `GET /actors`: Retrieves details for actors.
+        - `GET /directors`: Retrieves details for directors.
+        - `GET /titles/person`: Retrieves titles related to a person.
+        - `GET /person/details`: Retrieves detailed information about a person.
+        """
     },
     {
         "name": "Auth",
-        "description": "JWT Authenticated Path"
+        "description": "Endpoints requiring JWT authentication."
     },
     {
         "name": "User",
-        "description": "Yet to be added"
+        "description": """Endpoints related to user actions and authentication.
+        - `GET /user/rated_titles`: Retrieves the list of movies rated by the user.
+        - `POST /user/signup`: Registers a new user.
+        - `POST /user/signin`: Authenticates and provides an access token.
+        - `POST /user/rate`: Allows users to rate a specific movie.
+        - `GET /user/titles/{username}`: Retrieves movies rated by a specific user.
+        """
     },
     {
         "name": "Episodes",
-        "description": "Yet to be done"
+        "description": """Endpoints related to episodes and TV shows.
+        - `GET /title/episodes`: Retrieves information about episodes.
+        - `GET /title/tv_show/og_title`: Retrieves TV shows by original title.
+        - `GET /title/tv_show/start_year`: Retrieves TV shows by the start year.
+        - `GET /title/tv_show/end_year`: Retrieves TV shows by the end year.
+        - `GET /title/tv_show/genre`: Retrieves TV shows by genre.
+        """
+    },
+    {
+        "name": "tv-shows",
+        "description": """Endpoints related to TV shows.
+        - `GET /title/tv_show/og_title`: Retrieves TV shows by original title.
+        - `GET /title/tv_show/start_year`: Retrieves TV shows by the start year.
+        - `GET /title/tv_show/end_year`: Retrieves TV shows by the end year.
+        - `GET /title/tv_show/genre`: Retrieves TV shows by genre.
+        """
     }
 ]
